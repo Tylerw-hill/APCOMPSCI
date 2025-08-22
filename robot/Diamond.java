157d3b37a3dd547ee45f6c7cd02479a595ca76d5
Tylerw-hill <wonghill.styling@gmail.com>
2025-08-22 21:10:54 +0000

    Too large to do for loops so smaller scale

--- robot/Diamond.java
+++ robot/Diamond.java
@@ -9,24 +9,263 @@ public class Diamond implements Directions{
         World.setVisible(true);// allows us to see the run output
         // the bigger the street, the farther north
         World.setSize(20,20);
+        World.setDelay(3);
 
 
         // The line below creates a Robot that we will refer to as rob
         // Find out what the numbers and direction do!
         // Put rob in a better location for your initials.
-        Robot rob = new Robot(15,2,South,9);
+        Robot rob = new Robot(10,19,North,80);
 
         // Want a second robot?  No prob.  They are cheap :)
         //Robot dude = new Robot(7,5,West,9);
-        // examples of commands you can invoke on a Robot
-        rob.move();// move one step in the direction it is facing
 
+        //top right side of the diamond
+        rob.putBeeper();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+       
+        //top left side of the diamond
 
-        // starting the letter R
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
         rob.putBeeper();
 
-
-
+        //bottom left side of Diamond
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        
+        //Bottom Right side of diamond
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+         rob.move();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+       
         // done with the line, now on the curve
         // rob.turnLeft();
 
