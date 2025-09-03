727c7331357e49f02f97246168e75e7b05131c3f
Tylerw-hill <wonghill.styling@gmail.com>
2025-09-03 21:51:09 +0000

    I added a function that both moves and checks if thers a beeper there.

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -14,53 +14,59 @@ public class Roomba implements Directions {
         System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
     }
 
-    // declared here so it is visible in all the methods!
+
     private Robot roomba;
     private int totalBeepers = 0; 
     int i = 0;
 
     public int cleanRoom(String worldName, int startX, int startY) {
-        // Load world first
+
         World.readWorld(worldName);
         World.setVisible(true);
+		World.setDelay(10);
 
-        // assign to instance variable
         roomba = new Robot(startX, startY, East, 80);
 
-        // === your movement loop (fixed condition) ===
-        for (i = 0; i <= 2; i++) {   // was i >= 2 (never ran)
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
+        for (i = 0; i <= 2; i++) {
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
             roomba.turnLeft();
-            roomba.move();
+            moveAndPick();
             roomba.turnLeft();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.move();
-            roomba.turnLeft();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
+            moveAndPick();
             roomba.turnLeft();
             roomba.turnLeft();
-            roomba.move();
+            roomba.turnLeft();
+            moveAndPick();
             roomba.turnLeft();
             roomba.turnLeft();
             roomba.turnLeft();
         }
 
-        // === fixed beeper picking loop ===
+
         while (roomba.nextToABeeper()) {
             roomba.pickBeeper();
             totalBeepers++;
         }
 
         return totalBeepers;
+    }
+    private void moveAndPick() {
+        roomba.move();
+        while (roomba.nextToABeeper()) {
+            roomba.pickBeeper();
+            totalBeepers++;
+        }
     }
 }
