fa72ac8b34056cb2971fe760f25a4a74d724a33d
Tylerw-hill <wonghill.styling@gmail.com>
2025-09-10 01:25:02 +0000

    researched n added the beeper pile part

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -1,77 +1,37 @@
 package robot;
 
-
 import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-    // Main method to make this self-contained
     public static void main(String[] args) {
-        // LEAVE THIS ALONE!!!!!!
         String worldName = "robot/TestWorld-2.wld";
 
         Roomba cleaner = new Roomba();
         int totalBeepers = cleaner.cleanRoom(worldName, 15, 15);
         System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-        System.out.println("The largest pile of beepers is " + totalBeepers + " beepers");
+        cleaner.biggestPile();
     }
-
 
     private Robot roomba;
     private int totalBeepers = 0; 
     int i = 0;
-    int largestPile = 0;
-    int b = 0;
+
+    private int maxPileCount = 0;
+    private int maxPileStreet = -1;
+    private int maxPileAvenue = -1;
 
     public int cleanRoom(String worldName, int startX, int startY) {
-
         World.readWorld(worldName);
         World.setVisible(true);
-		World.setDelay(1);
-        World.setSize(50,50);
-
-        roomba = new Robot(startX, startY, South, 0);
-
-
-
-/*package robot;
-
-
-import kareltherobot.*;
-
-public class Roomba implements Directions {
-
-    // Main method to make this self-contained
-    public static void main(String[] args) {
-        // LEAVE THIS ALONE!!!!!!
-        String worldName = "robot/TestWorld-2.wld";
-
-        Roomba cleaner = new Roomba();
-        int totalBeepers = cleaner.cleanRoom(worldName, 15, 15);
-        System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-        System.out.println("The largest pile of beepers is " + totalBeepers + " beepers");
-    }
-
-
-    private Robot roomba;
-    private int totalBeepers = 0; 
-    int i = 0;
-    int largestPile = 0;
-    int b = 0;
-
-    public int cleanRoom(String worldName, int startX, int startY) {
-
-        World.readWorld(worldName);
-        World.setVisible(true);
-		World.setDelay(1);
+        World.setDelay(1);
         World.setSize(50,50);
 
         roomba = new Robot(startX, startY, South, 0);
         while(roomba.frontIsClear()){
             roomba.move();
         }
-        
-        
+
         roomba.turnLeft();
         roomba.turnLeft();
         roomba.turnLeft();
@@ -80,57 +40,64 @@ public class Roomba implements Directions {
         }
         roomba.turnLeft();
         roomba.turnLeft();
-        
-        
+            
         for (int i = 0; i < 23; i++) {
             twoRows();
         }
-        while (roomba.nextToABeeper()) {
-            roomba.pickBeeper();
-            totalBeepers++;
-        }
-        
-        return totalBeepers;
-    
-    }
-    private void moveAndPick() {
-        
-        while(roomba.frontIsClear()){
-        roomba.move();
-        while (roomba.nextToABeeper()) {
-            roomba.pickBeeper();
-            totalBeepers++;
-            if (b > largestPile ){
-	        largestPile = i;
-            }
 
-                    }
-            }  
+        collectPile();
+
+        return totalBeepers;
+    }
+
+    private void collectPile() {
+        int pileCount = 0;
+        while (roomba.nextToABeeper()) {
+            roomba.pickBeeper();
+            totalBeepers++;
+            pileCount++;
+        }
+        if (pileCount > maxPileCount) {
+            maxPileCount = pileCount;
+            maxPileStreet = roomba.street();
+            maxPileAvenue = roomba.avenue();
         }
     }
-    private void moveOne() {
-         roomba.move();
-            if(roomba.nextToABeeper()){
-                while(roomba.nextToABeeper()){
-                    roomba.pickBeeper();
-                    totalBeepers++;
-                    
-                }
-            }
+
+    private void moveAndPick() {
+        while(roomba.frontIsClear()){
+            roomba.move();
+            collectPile();
+        }
     }
+
+    private void moveOne() {
+        roomba.move();
+        collectPile();
+    }
+
     private void twoRows() {
-            moveAndPick();
-            roomba.turnLeft();
-            moveOne();
-            roomba.turnLeft();
-            moveAndPick();
-            roomba.turnLeft();
-            roomba.turnLeft();
-            roomba.turnLeft();
-            moveOne();
-            roomba.turnLeft();
-            roomba.turnLeft();
-            roomba.turnLeft();
+        moveAndPick();
+        roomba.turnLeft();
+        moveOne();
+        roomba.turnLeft();
+        moveAndPick();
+        roomba.turnLeft();
+        roomba.turnLeft();
+        roomba.turnLeft();
+        moveOne();
+        roomba.turnLeft();
+        roomba.turnLeft();
+        roomba.turnLeft();
+    }
+
+    public void biggestPile() {
+        if (maxPileCount == 0) {
+            System.out.println("No beepers were found.");
+        } 
+        else{
+            System.out.println("The biggest pile was at (" + maxPileStreet + ", " +
+                               maxPileAvenue + ") with " + maxPileCount + " beepers.");
+        }
     }
 }
-*/
