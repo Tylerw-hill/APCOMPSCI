682a3862de33d2237b7e32d67f457e1fc8bc999f
Tylerw-hill <wonghill.styling@gmail.com>
2025-11-17 21:42:34 +0000

    I finished the card class

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,19 @@
 package cards;
 
 public class Card {
-
+private int value;
+private String suit;
+private static String[] SUITS = {"♦","♣","♥","♠"};
+private static String[] VALUES = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}; 
+    public Card (int value, String suit){
+        this.value = value;
+        this.suit = suit;
+    }
+public int getValue(){
+    return value;
+}
+public String toString(){
+    return "" + SUITS[suit] + VALUES[value];
+}
 
 }
