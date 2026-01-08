5d1d0c0f412ebc17ff2f4df8084ad17270df6c86
Tylerw-hill <wonghill.styling@gmail.com>
2026-01-08 04:04:03 +0000

    I finished the Project Euler problem (#1)

---
+++ projectEuler.java
@@ -0,0 +1,10 @@
+public class projectEuler {
+    public static void main(String[] args) {
+        int count = 0;
+        for(int i = 0; i < 1000; i++){
+            if(i % 5 == 0 || i % 3 == 0)
+                count += i;
+        }
+        System.out.println(count);
+    }
+}
