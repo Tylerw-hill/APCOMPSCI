71661750bff11d073b67444ad7248698f3cfd904
Tylerw-hill <wonghill.styling@gmail.com>
2026-01-28 23:06:04 +0000

    changed the bubble part and added the switching component

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,16 +4,11 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
-         for (int outer = 0; outer < input.length - 1; outer++) {
-            // Inner loop is one shorter for every outer loop.
-            // Each loop ensures that the largest item moves to the end.
-            for (int inner = 0; inner < input.length - outer - 1; inner++) {
-                // If the two elements are out of order, swap them.
-                if (input[inner] > (input[inner + 1])) {
-                    int temp = input[inner];
-                    input[inner] = input[inner + 1];
-                    input[inner + 1] = temp;
-                }
+         for (int i = 0; i < input.length - 1; i++) {
+            while(input[i+1] > input[i]){
+                int temp = input[i + 1];
+                input[i + 1] = input[i];
+                input[i] = temp;
             }
         }
     }
