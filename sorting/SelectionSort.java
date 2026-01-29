6a3fa3b2b714e07a988de03352d219ef2050093b
Tylerw-hill <wonghill.styling@gmail.com>
2026-01-29 03:21:01 +0000

    added loops to selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,17 +4,11 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
-         for (int outer = 0; outer < input.length - 1; outer++) {
-            // Inner loop is one shorter for every outer loop.
-            // Each loop ensures that the largest item moves to the end.
-            for (int inner = 0; inner < input.length - outer - 1; inner++) {
-                // If the two elements are out of order, swap them.
-                if (input[inner] > (input[inner + 1])) {
-                    int temp = input[inner];
-                    input[inner] = input[inner + 1];
-                    input[inner + 1] = temp;
-                }
-            }
+         for (int i = 0; i < input.length; i++) { //goest through each term
+            int smallest = input[i];
+            for (int v = 0; v < input.length; v++){ // this is the part that looks through term and compares to another 
+            
+           }
         }
     }
 }
