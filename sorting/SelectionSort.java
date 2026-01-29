69e553b79af6f8d3b99da2e66e00d8a0f807d78d
Tylerw-hill <wonghill.styling@gmail.com>
2026-01-29 03:26:06 +0000

    I think the smallest variable is in the right spot so im gonna commit

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -5,9 +5,12 @@ public class SelectionSort implements Sorter {
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
          for (int i = 0; i < input.length; i++) { //goest through each term
-            int smallest = input[i];
+            int smallest = i;
             for (int v = 0; v < input.length; v++){ // this is the part that looks through term and compares to another 
-            
+                
+                if(input[v] > input[smallest]){
+                    smallest = v;
+                }
            }
         }
     }
