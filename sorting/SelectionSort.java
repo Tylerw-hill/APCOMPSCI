bcfe7ac36643bcb0494e01c58120b66f5e885797
Tylerw-hill <wonghill.styling@gmail.com>
2026-01-30 05:25:45 +0000

    I think the code works but i need to make the random number generator

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -6,9 +6,9 @@ public class SelectionSort implements Sorter {
         System.out.println("Selection Sort!!!");
          for (int i = 0; i < input.length; i++) { //goest through each term
             int smallest = i;
-            for (int v = 0; v < input.length; v++){ // this is the part that looks through term and compares to another 
+            for (int v = i + 1; v < input.length; v++){ // this is the part that looks through term and compares to another 
                 
-                if(input[v] > input[smallest]){
+                if(input[v] < input[smallest]){
                     smallest = v;
                 }
            }
